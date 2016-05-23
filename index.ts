efc43d3894a1d030d84fb8a13a28ffd29e9f3340
ShiningRay <tsowly@hotmail.com>
2016-05-24 05:01:58 +0800

    something

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import * as io from 'socket.io-client';
-const MAX_SAFE_INTEGER = 9007199254740990
+import * as Promise from 'bluebird'
+const MAX_SAFE_INTEGER = 9007199254740990;
 /**
- * 
+ *
  */
 export class BlankProxy {
   constructor(protected broker: Broker, protected objid: string) {
@@ -16,16 +17,57 @@ export class BlankProxy {
     return this;
   }
 }
-
+BlankProxy.prototype['subscribe'] = BlankProxy.prototype.on
+BlankProxy.prototype['addListener'] = BlankProxy.prototype.on
 /**
  * sync remote model data on server
  */
-export class Proxy extends BlankProxy {
+export class ModelProxy extends BlankProxy {
+  private value: any;
+  constructor(protected broker: Broker, protected objid: string) {
+    super(broker, objid);
+    this.on('update', this.handleUpdate.bind(this))
+  }
 
+  handleUpdate(keypath, value) {
+    this._set(keypath, value);
+  }
+
+  private _set(keypath, value) {
+    if (!keypath || keypath === '') {
+      this.value = value;
+      return;
+    }
+    if(!this.value){
+      this.value = {};
+    }
+    
+    let keypaths = keypath.split('.')
+    let ret = this.value;
+    let last = keypaths.pop();
+    keypaths.forEach((p) => {
+      if(!ret[p]){
+        ret[p] = {}
+      }
+      ret = ret[p];
+    })
+    ret[last] = value;
+  }
+
+  replace(data) {
+    this.value = data;
+  }
+  
+  /**
+   * return Rx.Observable
+   */
+  observe(event:string){
+    // return Rx.Observable
+  }
 }
 
 /**
- * define a struct for a pending request to server controller;  
+ * define a struct for a pending request to server controller;
  */
 interface RequestSpec {
   resolve: Function;
@@ -35,29 +77,29 @@ interface RequestSpec {
 
 /**
  * class: Broker(socket)
- * acts as a client endpoint for remote controller on server 
+ * acts as a client endpoint for remote controller on server
  */
 export class Broker {
   private __requests: { [id: number]: RequestSpec } = {};
-  private __events: any = {};
+  private __events: { [keypath: string]: { [eventName: string]: Array<Function> } } = {};
   private __reqId: number = 0;
   public onconnected: Function = this.noop;
 
   constructor(private socket: SocketIOClient.Socket) {
-    let self = this;
+
     socket.on('connect', () => this.onconnected())
       .on('event', (objid, eventName, args) => {
         // TODO: use pubsub to do event routing
-        let objevts = self.__events[objid];
+        let objevts = this.__events[objid];
         if (!objevts) {
           return console.log('no such object proxy')
         } else {
-          objevts = objevts[eventName];
-          if (!objevts) {
+          let handlers = objevts[eventName];
+          if (!handlers) {
             return
           }
-          objevts.forEach(function (h) {
-            return h.apply(null, args);
+          handlers.forEach(function (h) {
+            return h.apply(this, args);
           });
         }
       }).on('rpc:result', (reqId, result) => {
@@ -65,7 +107,7 @@ export class Broker {
         if (req) {
           req.resolve(result);
           clearTimeout(req.timeout);
-          delete self.__requests[reqId];
+          delete this.__requests[reqId];
         } else {
           console.log('req doesnt exist')
         }
@@ -93,26 +135,28 @@ export class Broker {
   /**
    * create a local proxy to sync with remote model
    */
-  proxy(objid) {
-    return new Proxy(this, objid);
+  getModel(keypath: string) {
+    let m = new ModelProxy(this, keypath);
+    this.invoke('getModel', [keypath]).then((data) => {
+      m.replace(data);
+    })
+    return m;
   }
 
   /**
    * send a "synchronic" request to remote controller and wait for a response
    */
-  invoke(method, args, timeout) {
-    let reqId = this.__reqId++, self = this;
-    if(reqId >= MAX_SAFE_INTEGER){
+  invoke(method: string, args, timeout: number = 5000) {
+    let reqId = this.__reqId++;
+    if (reqId >= MAX_SAFE_INTEGER) {
       this.__reqId = 0;
     }
-    if (!timeout) {
-      timeout = 5000;
-    }
-    console.log('invoke', method)
+
+    console.log('invoke', method);
     this.socket.emit('rpc:invoke', method, reqId, args);
     let timer = setTimeout(this.__handleTimeout, timeout, this, reqId);
     return new Promise((resolve, reject) => {
-      self.__requests[reqId] = {
+      this.__requests[reqId] = {
         resolve: resolve,
         reject: reject,
         timeout: timer
@@ -123,11 +167,11 @@ export class Broker {
   /**
    * subscribe specific event from a remote model
    */
-  subscribe(objid: string, evt: string, handler: (...any) => any): this {
-    this.socket.emit('subscribe', objid, evt);
-    let objevents = this.__events[objid];
+  subscribe(keypath: string, evt: string, handler: Function): this {
+    this.socket.emit('subscribe', keypath, evt);
+    let objevents = this.__events[keypath];
     if (!objevents) {
-      objevents = this.__events[objid] = {};
+      objevents = this.__events[keypath] = {};
     }
     if (!objevents[evt]) {
       objevents[evt] = [];
@@ -136,9 +180,24 @@ export class Broker {
 
     return this;
   }
+  
+  
 
   /**
-   * handle request timeout error; 
+   *
+   */
+  unsubscribe(keypath: string, evt: string, handler: Function): this {
+    this.socket.emit('subscribe', keypath, evt);
+    let objevents = this.__events[keypath];
+    if (objevents && objevents[evt]) {
+      let i = objevents[evt].indexOf(handler);
+      objevents[evt].splice(i, 1);
+    }
+    return this;
+  }
+
+  /**
+   * handle request timeout error;
    */
   __handleTimeout(broker: Broker, reqId: number) {
     let req = broker.__requests[reqId];
@@ -149,17 +208,23 @@ export class Broker {
   }
 }
 
-
-
-export var Client = {
-  connect: function (address: string, callback: Function) {
+export class Client {
+  private socket:SocketIOClient.Socket;
+  private controller:Broker;
+  private initialized: boolean=false;
+  
+  constructor(address: string, callback: Function) {
     this.socket = io(address);
     this.controller = new Broker(this.socket);
-    this.client.on('initialized', () => {
+    this.socket.on('initialized', () => {
       if (!this.initialized) {
         this.initialized = true;
         callback(this.controller);
       }
     });
+  }
+  
+  static connect (address: string, callback: Function):Client {
+    return new Client(address, callback);
   }
 }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="globals/shortid/index.d.ts" />
 /// <reference path="globals/socket.io-client/index.d.ts" />
+/// <reference path="modules/bluebird/index.d.ts" />
